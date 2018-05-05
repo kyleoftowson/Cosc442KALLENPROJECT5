033a42fa415d79f624c6495242fdebbc2a398383
Kallen21@students.towson.edu <kallen21@students.towson.edu>
2018-05-05 15:13:18 -0400

    Regex Changed
    I changed the pattern so that works now. The only thing t=doesnt work is the junit cases because something was wrong with my original FSM.  I cant figure out what was wrong with it.

--- Project5/src/JamesBond.java
+++ Project5/src/JamesBond.java
@@ -3,8 +3,10 @@ import java.util.regex.Pattern;
 public class JamesBond {
 	public Boolean bondRegex(String input){
 		// String to be scanned to find the pattern.
-	      
-	      String pattern = ".+(.+007.+).+";
+	     
+	      input=input.replace("(", "Z");
+	      input=input.replace(")", "z");
+	      String pattern = ".*Z.*007.*z.*";
 
 	      
 	      Pattern r = Pattern.compile(pattern);
